738ff34b2f0ebb16fd6bd960c8ef0bf9ac4560f0
Jess Draper <1527460@stu.tmc.ac.uk>
2025-12-04 10:55:13 +0000

    moved js to separate file + added header & footer to form page + changed some font sizes !!

---
+++ Bootstrap UX starter/js/form_validation.js
@@ -0,0 +1,62 @@
+function validate() {
+    let firstName = document.getElementById("name1").value;
+    let surname = document.getElementById("name2").value;
+    let emailAd = document.getElementById("email").value;
+    let number = document.getElementById("phone").value;
+    let educationName = document.getElementById("name3").value;
+    let reason = document.getElementById("comment").value;
+    let file = document.getElementById("myfile").value;
+
+    document.getElementById("finalmsg").innerHTML = "";
+    document.getElementById("demo1").innerHTML = "";
+    document.getElementById("demo2").innerHTML = "";
+    document.getElementById("demo3").innerHTML = "";
+    document.getElementById("demo4").innerHTML = "";
+    document.getElementById("demo5").innerHTML = "";
+    document.getElementById("demo6").innerHTML = "";
+    document.getElementById("demo7").innerHTML = "";
+    document.getElementById("demo8").innerHTML = "";
+
+    if (firstName === "") {
+        document.getElementById("demo1").innerHTML = "* Please fill in this field";
+        document.getElementById("finalmsg").innerHTML = "* There are errors in your form. Please review them before submission.";
+    }
+
+    if (surname === "") {
+        document.getElementById("finalmsg").innerHTML = "* There are errors in your form. Please review them before submission.";
+        document.getElementById("demo2").innerHTML = "* Please fill in this field";
+    }
+
+    if (emailAd === "") {
+        document.getElementById("finalmsg").innerHTML = "* There are errors in your form. Please review them before submission.";
+        document.getElementById("demo3").innerHTML = "* Please fill in this field";
+    }
+
+    atpos = emailAd.indexOf("@");
+    dotpos = emailAd.lastIndexOf(".");
+
+    if (atpos < 1 || (dotpos - atpos < 2)) {
+        document.getElementById("finalmsg").innerHTML = "* There are errors in your form. Please review them before submission.";
+        document.getElementById("demo8").innerHTML = "* Invalid input";
+    }
+
+    if (number === "") {
+        document.getElementById("finalmsg").innerHTML = "* There are errors in your form. Please review them before submission.";
+        document.getElementById("demo4").innerHTML = "* Please fill in this field";
+    }
+
+    if (educationName === "") {
+        document.getElementById("finalmsg").innerHTML = "* There are errors in your form. Please review them before submission.";
+        document.getElementById("demo5").innerHTML = "* Please fill in this field";
+    }
+
+    if (reason === "") {
+        document.getElementById("finalmsg").innerHTML = "* There are errors in your form. Please review them before submission.";
+        document.getElementById("demo6").innerHTML = "* Please fill in this field";
+    }
+
+    if (file === "") {
+        document.getElementById("finalmsg").innerHTML = "* There are errors in your form. Please review them before submission.";
+        document.getElementById("demo7").innerHTML = "* Please choose a file";
+    }
+}
